0f8a63b09d9331d52e709802ba3d5c9f08cb8d8c
haytham taweel <haytham_r10_r9@hotmail.com>
2021-09-23 15:16:40 +0300

    Add project files.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+chromewebdriver_location="/Users/hayth/source/repos/PythonApplication2/chromedriver"
+
+driver = webdriver.Chrome(chromewebdriver_location)
+driver.get("https://info.braude.ac.il/yedion/fireflyweb.aspx?prgname=login")
+
+#username and password variables and login button
+username_input=
+password_input=
+login_submit
