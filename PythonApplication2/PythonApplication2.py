cee9863e84d3bd8556af35937672fedbb45d124e
haytham taweel <haytham_r10_r9@hotmail.com>
2021-09-23 15:33:41 +0300

    updated

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -5,7 +5,13 @@ chromewebdriver_location="/Users/hayth/source/repos/PythonApplication2/chromedri
 driver = webdriver.Chrome(chromewebdriver_location)
 driver.get("https://info.braude.ac.il/yedion/fireflyweb.aspx?prgname=login")
 
-#username and password variables and login button
-username_input=
-password_input=
-login_submit
+#username and password variables and login button paths
+username_input="/html/body/main/div[3]/div/div/div/form/div/section/div/div/div/div/div[1]/input"
+password_input="/html/body/main/div[3]/div/div/div/form/div/section/div/div/div/div/div[2]/input"
+login_submit="/html/body/main/div[3]/div/div/div/form/div/section/div/div/div/div/div[3]/button[1]"
+
+#login page submit information and enter
+driver.find_element_by_xpath(username_input).send_keys("Haytham.Taweel")
+driver.find_element_by_xpath(password_input).send_keys("Haytham123!")
+driver.find_element_by_xpath(login_submit).click()
+
